6295a9f28b68a3a5ffe678cb6b7078010493cc93
NO8ODY <44928989+no8ody@users.noreply.github.com>
2021-02-15 14:06:40 +0100

    -

--- bee-chrysalis/update-bee.sh
+++ bee-chrysalis/update-bee.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 red='\033[1;31m'
+green='\033[1;32'
 nc='\033[0m'
-
 status="$(systemctl show -p ActiveState --value bee)"
 if [ "$status" != "active" ]; then
     cd /var/lib/bee
@@ -17,11 +17,16 @@ if [ "$status" != "active" ]; then
         rm -rf /var/lib/bee/target/release/storage/alphanet /var/lib/bee/target/release/snapshots/alphanet
         mv /var/lib/bee/target/release/config.toml /var/lib/bee/target/release/config.toml.bak
         cp -r /var/lib/bee/bee-node/config.example.toml /var/lib/bee/target/release/config.toml
+        privkey=$(cat /var/lib/bee/target/release/config.toml.bak | grep "identity_private_key")
+        sed -i 's/^.*identity_private_key.*$/'"$privkey"'/' /var/lib/bee/target/release/config.toml
         echo ""
-        echo -e $red "Bee successfully updated!" $nc
+        echo -e $green "Bee successfully updated!" $nc
+        echo ""
+        echo -e $red "Dont forget to update your configuration file!"
+        echo ""
     else
         echo ""
-        echo -e $red "Bee is already up2date!" $nc
+        echo -e $green "Bee is already up2date!" $nc
         echo ""
     fi
 else
